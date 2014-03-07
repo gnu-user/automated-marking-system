68716a5ac11518d07cea2d8c540527470af65f55
Joseph <joseph.heron@gmail.com>
2014-03-07 12:52:22 -0500

    ADDED: rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AutomatedMarkingSystem::Application.config.secret_key_base = '6fdd208872e22bd143adb7f3ee5cd8e8b14629c38ceeafd60401caef58e68aa4f496518a23f65fab8422d8db4a629a345200d8fcd8aef195b1cb5ed9efccf176'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AutomatedMarkingSystem::Application.config.session_store :cookie_store, key: '_automated-marking-system_session'
