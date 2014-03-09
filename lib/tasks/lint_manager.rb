2e4f1ab96724484ff29135867086776153c03968
capncanuck <khalil.fazal.0@gmail.com>
2014-03-09 05:39:25 -0400

    autoformatted files

--- lib/tasks/lint_manager.rb
+++ lib/tasks/lint_manager.rb
@@ -3,51 +3,51 @@ require_relative 'static_issues'
 
 class LintManager < Manager
 
-    # Format [relative_path/filename]: (issue_flag) description...
-    LINE_PARSER = /\[(.+):([0-9]+)\]: \((.+)\) (.*)/
+  # Format [relative_path/filename]: (issue_flag) description...
+  LINE_PARSER = /\[(.+):([0-9]+)\]: \((.+)\) (.*)/
 
-    COMMAND = "cppcheck"
-    OPTIONS = "--enable=all --force"
-    OUTPUT = "2>"
-    FILE = "temp.txt"
+  COMMAND = 'cppcheck'
+  OPTIONS = '--enable=all --force'
+  OUTPUT = '2>'
+  FILE = 'temp.txt'
 
-    def applyLint()
-        process()
+  def applyLint
+    process
+  end
+
+  def process
+    super
+
+    @output = Array.new
+    %x(#{COMMAND} #{OPTIONS} #{@path_to_file} #{OUTPUT} #{FILE})
+
+    File.open(FILE, 'r').each_line do |line|
+      @output.push(line)
     end
 
-    def process()
-        super
-        
-        @output = Array.new
-        %x(#{COMMAND} #{OPTIONS} #{@path_to_file} #{OUTPUT} #{FILE})
-            
-        File.open(FILE, "r").each_line do |line|
-            @output.push(line)
-        end
+    # Delete the temp file created
+    File.delete(FILE)
+    @output
+  end
 
-        # Delete the temp file created
-        File.delete(FILE)
-        @output
+  def parseOutput
+
+    issuesList = Array.new
+    @output.each do |line|
+      parsed = line.scan(LINE_PARSER)
+
+      # There should only be 1 value
+      if parsed.size == 1
+        filename, line, type, description = parsed[0]
+        issuesList.push(StaticIssues.new(filename, line, type, description))
+      end
     end
 
-    def parseOutput()
-
-        issuesList = Array.new
-        @output.each do |line|
-            parsed = line.scan(LINE_PARSER)
-
-            # There should only be 1 value
-            if parsed.size == 1
-                filename, line, type, description = parsed[0]
-                issuesList.push(StaticIssues.new(filename, line, type, description))
-            end
-        end
-
-        return issuesList
-    end
+    issuesList
+  end
 end
 
-manager = LintManager.new("example_programs/example.cpp")
+manager = LintManager.new('example_programs/example.cpp')
 
 #output = Array.new
 
@@ -57,12 +57,12 @@ manager = LintManager.new("example_programs/example.cpp")
 
 #manager.output = output
 
-manager.applyLint()
+manager.applyLint
 
 issuesList = manager.parseOutput
 
 if issuesList.size > 0
-    puts issuesList
+  puts issuesList
 else
-    puts "No issues found"
+  puts 'No issues found'
 end
--- lib/tasks/manager.rb
+++ lib/tasks/manager.rb
@@ -2,22 +2,22 @@
 class Manager
     attr_accessor :path_to_file, :output
 
-    def initialize(path="")
+    def initialize(path='')
         @path_to_file = path
     end
 
-    def checkFileExist()
-        File.exist?(@path_to_file)
+    def checkFileExist
+      File.exist?(@path_to_file)
     end
 
-    def process()
-        if !checkFileExist()
-            puts "File not found"
-            return Array.new
-        end
+    def process
+      unless checkFileExist
+        puts 'File not found'
+        Array.new
+      end
     end
 
-    def parseOutput()
+    def parseOutput
 
     end
 end
