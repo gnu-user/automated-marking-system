3c3d2f69ebc454b827fc6c696f4ea2c1797c9fcd
Joseph <heron.joseph@gmail.com>
2014-04-03 17:39:08 -0400

    ADDED: paths for student

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,5 +1,14 @@
 class StudentController < ApplicationController
+  layout "code", only: [:show]
+
   def index
     @title = 'Student'
+  end
+
+  def assignment 
+  end
+
+  def show
+
   end
 end
