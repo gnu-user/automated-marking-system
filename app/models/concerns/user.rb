ce921ae9149605515235d0c93e6a8b2ab348516e
capncanuck <khalil.fazal.0@gmail.com>
2014-04-06 18:34:44 -0400

    modularized Student and Admin models

---
+++ app/models/concerns/user.rb
@@ -0,0 +1,31 @@
+module User extend ActiveSupport::Concern
+  included do
+    attr_accessor :password, :password_confirmation
+    before_save :encrypt_password
+    after_save :clear_password
+
+    validates_confirmation_of :password
+    validates_presence_of :password, on: :create
+  end
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def clear_password
+    self.password = nil
+    self.password_confirmation = nil
+  end
+end
