34628dfabc91ec32fa0f0e2a461c1fae4020dbd0
Joseph <heron.joseph@gmail.com>
2014-04-06 22:46:02 -0400

    FIXED: logout issue

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -6,22 +6,25 @@ $(document).ready(function () {
 	$('#add_eval').click(function(event) {
 		alert("Add eval clicked, Don't Click Me!");
 
+		// Save the values stored in the test function for evaluation
+
 		return false;
 	});
 	$('#upload_eval').click(function(event) {
 		alert("Upload eval clicked, Don't Click Me!");
 
+		// Accept an upload for the test cases to be parsed for evaluation
 		return false;
 	});
 
 	$('#add_sample').click(function(event) {
 		alert("Add sample clicked, Don't Click Me!");
-
+		// Save the values stored in the test function for sample
 		return false;
 	});
 	$('#upload_sample').click(function(event) {
 		alert("Upload sample clicked, Don't Click Me!");
-
+		// Accept an upload for the test cases to be parsed for sample
 		return false;
 	});
 
