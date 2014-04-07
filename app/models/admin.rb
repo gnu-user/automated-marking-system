78efee52452b66f4ce643f410ddc45fe7d7378b5
capncanuck <khalil.fazal.0@gmail.com>
2014-04-06 20:38:47 -0400

    couldn't get class methods to work in module

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,13 @@ class Admin < ActiveRecord::Base
 
   validates_presence_of :prof_id
   validates_uniqueness_of :prof_id
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
 end
