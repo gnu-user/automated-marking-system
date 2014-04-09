9a0e113cc6a6dac82349e75785f761b08cc3d95d
gnu_user <gsoc.student@gmail.com>
2014-04-09 19:35:02 -0400

    GPLd

--- test/models/test_test.rb
+++ test/models/test_test.rb
@@ -1,3 +1,30 @@
+##############################################################################
+#
+# Automated Marking System (AMS)
+# 
+# A scalable automated marking system that provides automated marking, quality
+# feedback, and cheating detection all in one easy to use solution.
+#
+#
+# Copyright (C) 2014, Joseph Heron, Jonathan Gillett, Daniel Smullen, 
+# and Khalil Fazal
+# All rights reserved.
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 require 'test_helper'
 
 class TestTest < ActiveSupport::TestCase
